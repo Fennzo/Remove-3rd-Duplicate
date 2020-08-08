d5747b516a7aa5dac926c19ae98ffd6e74526591
Alex Jin <yijian36@gmail.com>
2020-08-09 02:11:47 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,57 @@
+package com.selenium.test;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class App 
+{
+	public static char[] remove(int index, char[] arr) {
+	    char[] newArr = new char[arr.length - 1];
+	    if(index < 0 || index > arr.length) {
+	        return arr;
+	    }
+	    int j = 0;
+	    for(int i = 0; i < arr.length; i++) {
+	        if(i == index) {
+	            i++;
+	        }
+	        newArr[j++] = arr[i];
+	    }
+
+	    return newArr;
+	}
+    public static void main( String[] args ) throws ParseException {
+    	Scanner in = new Scanner(System.in);
+    	String str;
+    	
+    	System.out.print("Enter str: ");
+    	str = in.next();
+    	
+    	
+        char[] temp = str.toCharArray();
+        char[] temp1 = str.toCharArray();
+        temp = remove(5, temp1);
+        System.out.println(Arrays.toString(temp1) + temp1.length);
+        int length = temp.length;
+        for ( int i = 0; i < length-1; i++) {
+        	int count = 0;
+        	for ( int j = i+1; j < length; j++) {
+        		if ( temp[i] == temp[j]) {
+        			count++;
+        			if ( count > 0) {
+        				temp = Arrays.copyOf(temp, temp.length+1);
+        				temp = remove(j, temp);
+        				length--;
+        			}
+        		}
+        	}
+        	
+        }
+        
+       String result = String.valueOf(temp);
+       System.out.println(result);
+    	in.close();
+		}}
